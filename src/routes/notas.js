dbe3f14c4cc6e5aa7191d85e7c6ce2a501200b2b
Federico-Benitez <fedebenitez1996@gmail.com>
2021-03-06 12:22:17 -0300

    ruta de obtener todas las notas agregada

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 const dbConnection = require('../database');
 
-
+router.get('/all', (req,res) => {
+    dbConnection.query('SELECT * FROM notas',(err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        }else {
+            console.log("error no se pudieron obtener las notas")
+        }
+    });
+})
 
 
 module.exports= router;
