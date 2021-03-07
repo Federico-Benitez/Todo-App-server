ad866a17f7094375f046bef0d5b0b0f6a62ed112
Federico-Benitez <fedebenitez1996@gmail.com>
2021-03-07 10:01:34 -0300

    modificaciones en el get all y post de nota agregado

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -3,25 +3,50 @@ const router = express.Router();
 
 const dbConnection = require('../database');
 
-router.get('/all', (req,res) => {
-    dbConnection.query('SELECT * FROM notas',(err, rows, fields) => {
-        if(!err){
-            res.json(rows);
-        }else {
-            console.log("error no se pudieron obtener las notas")
-        }
-    });
-})
+router.get('/all', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  dbConnection.query('SELECT * FROM notas', (err, rows) => {
+    if (err) throw err;
+    console.log(rows);
+    res.json(rows);
+  });
+});
 
-router.get('/:id',(req,res) => {
-    const {id} = req.params;
-    dbConnection.query('SELECT * FROM notas WHERE id=?',[id],(err,rows,fields)=>{
-        if(!err){
-            res.json(rows[0]);
-        }else{
-            console.log("nota no encontrada");
-        }
-    })
-})
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  dbConnection.query(
+    'SELECT * FROM notas WHERE id=?',
+    [id],
+    (err, rows, fields) => {
+      if (err) throw err;
+      if (!err) {
+        res.json(rows[0]);
+      } else {
+        //TODO: Arreglar, arrojar 404 en caso de no encontrar
+        console.log('nota no encontrada');
+      }
+    }
+  );
+});
 
-module.exports= router;
+router.post('/', (req, res) => {
+  const query = 'INSERT INTO notas SET ?';
+
+  const { contenido } = req.body;
+  const nota = {
+    contenido: contenido,
+    state: false
+  };
+
+  dbConnection.query(query, nota, (error) => {
+    if (error) throw error;
+    res.json({ Status: 'Nota agregada' });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const query = 'DELETE * FROM notas WHERE id=?';
+  res.send('eliminado');
+});
+
+module.exports = router;
